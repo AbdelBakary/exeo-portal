05d06346554336a1d39cc94630e4145893ce6622
Imane Bakary <imanebakary@MacBook-Air-3.local>
2025-09-22 11:57:50 +0200

    Add migrations to startup script

--- start.sh
+++ start.sh
@@ -3,6 +3,10 @@
 # Set default port if PORT is not set
 PORT=${PORT:-8000}
 
+# Run migrations
+echo "Running Django migrations..."
+python manage.py migrate
+
 # Start the Django application
 echo "Starting Django application on port $PORT"
 exec gunicorn --bind 0.0.0.0:$PORT --workers 3 exeo_portal.wsgi:application
